2fcce1ca341880dc0b22457c117dde6da1650a2c
mark <mark@mark>
2015-04-30 21:33:45 +0200

    - inititated master plugin for 'pilzdatenbank' - created custom post type

---
+++ plugins/gcms_pilzdb/gcms_pilzdb.php
@@ -0,0 +1,36 @@
+<?php
+    /**
+     * Plugin Name: Pilzdatenbank
+     * Description: Hauptplugin, welches Pilzdatenbank Funktionalitäten zur Verfügung stellt
+     * Version: 0.1
+     * Author: Grundlagen CMS
+     */
+
+    class pilzDb {
+
+        const _POST_TYPE_NAME = "pilze";
+
+        public function __construct() {
+            add_action('init', array($this, 'createCustomPostType'));
+        }
+
+        public function createCustomPostType() {
+            register_post_type(
+                self::_POST_TYPE_NAME,
+                array(
+                    'labels' => array(
+                        'name' => ucfirst(self::_POST_TYPE_NAME),
+                    ),
+                    'public' => true,
+                    'show_ui' => true,
+                    'query_var' => strtolower(self::_POST_TYPE_NAME)
+                )
+            );
+
+            //Custom Hook after registering Posttype
+            do_action('pilzdatenbank_posttype_created');
+
+        }
+    }
+
+    $pilzDb = new pilzDb();

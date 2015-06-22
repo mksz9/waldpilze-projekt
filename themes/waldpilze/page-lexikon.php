3b563ed7267c816107c330520f5b0ee9cd91b4f3
Marcel <marcel.swoboda@gmx.de>
2015-06-23 00:15:31 +0200

    theme links

--- themes/waldpilze/page-lexikon.php
+++ themes/waldpilze/page-lexikon.php
@@ -11,14 +11,14 @@ get_sidebar(); ?>
             <div class="row">
 
                 <div class="col-sm-12">
-                <?php if ( have_posts() ) : ?>
-                    <?php while ( have_posts() ) : // The Loop ?>
-                        <?php the_post(); ?>
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_content(); ?>
-                    <?php endwhile; // end of one post ?>
-                <?php endif; // do not delete
-                wp_reset_query();?>
+                    <?php if (have_posts()) : ?>
+                        <?php while (have_posts()) : // The Loop ?>
+                            <?php the_post(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                        <?php endwhile; // end of one post ?>
+                    <?php endif; // do not delete
+                    wp_reset_query(); ?>
                 </div>
 
                 <?php
@@ -36,10 +36,13 @@ get_sidebar(); ?>
                     <div class="col-sm-4">
                         <a class="lexikon_link" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
                         <?php
-                        if (has_post_thumbnail()) {
-                            the_post_thumbnail('medium', array(
-                                'class' => "img-responsive ",
-                            ));
+                        if (has_post_thumbnail()) { ?>
+                            <a href="<?php the_permalink() ?>">
+                                <?php the_post_thumbnail('medium', array(
+                                    'class' => "img-responsive ",
+                                )); ?>
+                            </a>
+                        <?php
                         }
                         ?>
                     </div>
